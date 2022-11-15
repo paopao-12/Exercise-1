b771f4d3684bc1c626ff41f26b67df8340ce00cd
Jose Paolo Gigayon <97038833+paopao-12@users.noreply.github.com>
2022-11-15 18:07:13 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("Enter Username:")
+age = int(input("Enter Age:"))
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+print("The username is " ,name, " and the age is" ,str (age))
+print("The Product is ",str(num1*num2))
